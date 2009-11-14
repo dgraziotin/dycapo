745bd0c180b49cda5657492f3c5645906d7b15d8
Daniel Graziotin <daniel.graziotin@gmail.com>
2009-11-14 18:14:00 +0100

    Added rpc4django from http://www.davidfischer.name/rpc4django/

---
+++ rpc4django/rpc4django/tests/__init__.py
@@ -0,0 +1,8 @@
+from test_jsonrpcdispatcher import *
+from test_rpcdispatcher import *
+
+# This should only be executed from manage.py test rpc4django
+from test_rpcviews import *
+
+if __name__ == '__main__':
+    unittest.main()
