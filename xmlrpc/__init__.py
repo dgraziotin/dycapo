74dfcdad17935e7643a36811e77947c2477c82c8
BodomLx <daniel.graziotin@gmail.com>
2010-10-17 16:10:12 +0200

    Quite final cleanup of XMLRPC parts

--- xmlrpc/__init__.py
+++
@@ -1,4 +0,0 @@
-from driver import insertTrip, startTrip, getRides
-from driver import acceptRide, refuseRide, finishTrip
-from passenger import searchRide, requestRide, statusRequestedRide, cancelRide, startRide, finishRide
-from common import getPosition, register, changePassword, setPosition
